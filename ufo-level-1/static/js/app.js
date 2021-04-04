c8813639420eb391c575f74f945dd1a5b840aaa9
Davis Knowles <dknowles52@gmail.com>
2021-04-04 19:51:42 -0400

    add listener

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,3 +1,5 @@
+//AUTOMATIC TABLE AND DATE SEARCH
+
 // from data.js
 var tableData = data;
 
@@ -16,4 +18,40 @@ tableData.forEach((ufoSighting) => {
     });
   });
   
+//USE D3 TO ADD AN EVENT LISTENER
+
+  // Assign the data from `data.js` to a descriptive variable
+var UFO = data;
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+//   console.log(UFO);
+
+  var filteredData = UFO.filter(person => 
+    person.datetime === inputValue);
+
+  console.log(filteredData);
+
+}
 
